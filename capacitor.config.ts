f9051f9153f65350b831643ff393f0f89a8bcc67
almwebdatadev <almwebdatadev@gmail.com>
2025-10-27 11:46:22 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'cronometro',
+  webDir: 'www'
+};
+
+export default config;
